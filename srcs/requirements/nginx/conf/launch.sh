e65d045f6c711fd8ccc737fc4e19eded83e3d9a3
apetitco <apetitco@student.42.fr>
2025-09-25 18:12:58 +0200

    commit de gros porc

--- srcs/requirements/nginx/conf/launch.sh
+++ srcs/requirements/nginx/conf/launch.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-chown -R www-data:www-data /var/www/wordpress
+chown -R www-data:www-data /var/www/html
 sed -i /etc/nginx/nginx.conf -e 's/#SERVER_NAME_HERE#/server_name '"$DOMAIN www.$DOMAIN"/;
 NAMESERVER="$(awk '/^nameserver/{print $2}' /etc/resolv.conf)"
 sed -i /etc/nginx/nginx.conf -e 's/#RESOLVER_HERE#/resolver '"$NAMESERVER"'/'
